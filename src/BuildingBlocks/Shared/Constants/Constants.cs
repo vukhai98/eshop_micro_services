901efd18dc8c3553a7fefcc0655858d767a886ce
RUNSYSTEM\khaivm <khaivm@runsystem.net>
2025-03-19 23:17:58 +0700

    create serilog

---
+++ src/BuildingBlocks/Shared/Constants/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace Shared.Constants
+{
+    public static class Constants
+    {
+        public const string StartProject = "Starting project: {ProjectName} API up";
+        public const string ShutdownProject = "Shut down: {ProjectName} API complete";
+        public const string UnhandledException = " Unhandled exception";
+    }
+}
